68874b8d1badaab1aad6973d50d508078a8a5a0b
Halil I. ILHAN <hybrayhem@gmail.com>
2021-01-27 18:32:26 +0300

    receiver v2

--- serial-receiver.ino
+++ serial-receiver.ino
@@ -2,11 +2,13 @@ const int switchPin = 2;
 const int xAxis = A0;         
 const int yAxis = A1;         
 int lastSwitchState = LOW;
-int xMap, yMap, xValue, yValue;
+int xMap, yMap, old_xMap, old_yMap, xValue, yValue;
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(switchPin, INPUT_PULLUP);
+  old_xMap = 0;
+  old_yMap = 0;
   
   Serial.begin(9600);
   while (!Serial);
@@ -17,6 +19,7 @@ void loop() {
   if (Serial.available())
     {
       long int command = Serial.parseInt();
+      String str_command = String(command);
       if (command == 1)
       {
        digitalWrite(LED_BUILTIN, HIGH);
@@ -32,10 +35,10 @@ void loop() {
        led_blink(3, 300);
        Serial.println("blinked");
       }
-      else if (String(command).startsWith("4"))
+      else if (str_command.startsWith("4"))
       {
        delay(50);
-       long int number = String(command).substring(1).toInt();
+       long int number = str_command.substring(1).toInt();
        Serial.println(number*number);
       }
       else if (command == 5)
@@ -43,29 +46,36 @@ void loop() {
        delay(50); 
        Serial.println("BONUS!!!!");
        while(1){
-          int stop_command = Serial.parseInt();
-          if(stop_command == 6){
-            break;
-          }
-          
           int switchState = digitalRead(switchPin);
           if (switchState != lastSwitchState) {
             if (switchState == LOW) {
               Serial.println("+");
+            }
+            if (switchState == LOW && xMap == 1 && yMap == 5){
+              Serial.println("break");
+              break;
             }
           }else{
             xValue = analogRead(xAxis);
             yValue = analogRead(yAxis);
             xMap = map(xValue, 0,1023, 1, 9);
             yMap = map(yValue,0,1023,1,9);
-            
-            Serial.print(xMap);
-            Serial.println(yMap);
+            if(old_xMap != xMap || old_yMap != yMap){
+              Serial.print(xMap);
+              Serial.println(yMap);
+            }
+            old_xMap = xMap;
+            old_yMap = yMap;
           }
-//          delay(100);
+          delay(100);
           lastSwitchState = switchState;
        }
        Serial.println("end of bonus");
+      }
+      else if (str_command.startsWith("6")){
+        int times = str_command.substring(1).toInt();
+        led_blink(times, 300);
+        Serial.println("blinked");
       }
       else{
         if(command != 0){
@@ -74,7 +84,6 @@ void loop() {
         }
       }
     }
-
 }
 
 void led_blink(int times, int _delay){
